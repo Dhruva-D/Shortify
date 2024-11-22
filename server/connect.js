ccfc761a90b239f2ce5719e4225ed3597b1a01ab
Dhruva-D <dhruvad575@gmail.com>
2024-11-23 02:19:42 +0530

    Updated connect.js

--- server/connect.js
+++ server/connect.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectToMongoDB = async () => {
+const connectToMongoDB = async (url) => {  // Accept 'url' as a parameter
   if (mongoose.connection.readyState === 0) {
     await mongoose.connect(url, {
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 5000,  // Timeout after 5 seconds
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
