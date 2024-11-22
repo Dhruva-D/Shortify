fd090fab4cc76661b49bd67ea48bc5e51a4da1c0
Dhruva-D <dhruvad575@gmail.com>
2024-11-23 01:34:18 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: ["https://shortify-url-three.vercel.app"], 
+  origin: ["https://shortify-url-three.vercel.app/"], 
   methods: ["GET", "POST"],
   credentials: true,
 }));
